4c46bca8fb0334a657713a65a0a0cd0a11cff269
Preeyadarshee Dev <31573375+preeyadarshee123@users.noreply.github.com>
2020-11-11 10:19:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ vector<vector<ll>> matrixMultiply(vector<vector<ll>> a,vector<vector<ll>> b){
     vector<vector<ll>> c(sz,vector<ll> (sz,0));
     int n=(int)a.size();
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            for(int k=0;k<n;k++){
-                c[i][j]+=(a[i][k]*b[k][j])%inf;
-                c[i][j]%=inf;
+        for(int k=0;k<n;k++){
+            for(int j=0;j<n;j++){
+                c[i][j] += (a[i][k]*b[k][j]);
+                c[i][j] %= inf;
             }
         }
     }
